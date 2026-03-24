1bacb60bfa5dc16fe728c27e8b6baba4e20d2848
mngapps <alex.menges@yahoo.de>
2026-03-24 22:46:58 +0100

    enhanc ed by xai

---
+++ app/db/migrations/versions/0015_merge_heads.py
@@ -0,0 +1,19 @@
+"""Merge heads: 0012_add_xai_provider and 0014_robustness_fixes.
+
+Revision ID: 0015_merge_heads
+Revises: 0012_add_xai_provider, 0014_robustness_fixes
+Create Date: 2026-03-24
+"""
+
+revision = "0015_merge_heads"
+down_revision = ("0012_add_xai_provider", "0014_robustness_fixes")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
